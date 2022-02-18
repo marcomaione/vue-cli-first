46d3d078010dde73df7414791b198f923e3e81d2
marco maione <maionemarco14@gmail.com>
2022-02-18 17:29:04 +0100

    bonus

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <Title></Title>
+    <Hero></Hero>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    Title
+    Title,
+    Hero
   }
 }
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,10 @@
+<template>
+   <img src="https://www.crushpixel.com/big-static14/preview4/ciao-both-hello-bye-italian-1603811.jpg" alt="">
+</template>
+
+<script>
+export default {
+  name: 'HeroJpg',
+  
+}
+</script>
